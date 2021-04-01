aec3f5421422bb2643554ee75e0d97b7f8168930
Alex Zheng <alex.zhenghw@gmail.com>
2021-04-01 21:21:42 +1100

    alg27

---
+++ src/Alg27.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+let removeElement = function (nums, val) {
+  if (nums.length === 0) return 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
