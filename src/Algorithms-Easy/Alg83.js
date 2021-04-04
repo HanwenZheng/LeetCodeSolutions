32012506424995d91f2b47c2646c07f00e588261
Alex Zheng <alex.zhenghw@gmail.com>
2021-04-04 21:45:08 +1000

    alg 83

---
+++ src/Algorithms-Easy/Alg83.js
@@ -0,0 +1,17 @@
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+let deleteDuplicates = function (head) {
+  if (head === null) return head;
+  let node = head;
+  while (node !== null && node.next !== null) {
+    if (node.val === node.next.val) {
+      node.next = node.next.next;
+    } else {
+      node = node.next;
+    }
+  }
+  return head;
+};
